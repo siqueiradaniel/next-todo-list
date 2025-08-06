251d26915852fade3dc1d9ae01b8c703d48a3035
siqueiradaniel <danielsiqueira111@gmail.com>
2025-08-06 16:43:21 -0300

    design: implement responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import TaskCardList from "@/components/TaskCardList";
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-blue-300 flex items-center justify-center">
-      <div className="h-full min-w-xl flex flex-col items-center bg-gray-100 p-8 rounded-xl">
+      <div className="h-full max-w-2xs md:min-w-xl flex flex-col items-center bg-gray-100 p-8 rounded-xl">
         <Header />
         <AddTaskInput />
         <TaskCardList />
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ import { Notebook, Pencil } from 'lucide-react'
 const Header = () => {
     return (
         <div className='flex flex-row'>
-            <h1 className='text-3xl font-bold text-gray-700'>ToDo List</h1>
-            <Notebook className='ms-2 w-10 h-10 text-gray-700'/>
-            <Pencil className='w-10 h-10 text-gray-700'/>
+            <h1 className='text-2xl md:text-3xl font-bold text-gray-700'>ToDo List</h1>
+            <Notebook className='ms-2 w-7 h-7 md:w-10 md:h-10 text-gray-700'/>
+            <Pencil className='w-7 h-7 md:w-10 md:h-10 text-gray-700'/>
         </div>
     )
 }
