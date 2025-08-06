a0001c1f089dc3688bfa3c62e03d331fa0252f38
siqueiradaniel <danielsiqueira111@gmail.com>
2025-08-06 16:35:26 -0300

    refactor: transform input task to a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,13 @@
-"use client"
-
+import AddTaskInput from "@/components/AddTaskInput";
 import Header from "@/components/Header";
 import TaskCardList from "@/components/TaskCardList";
-import { useTasksContext } from "@/context/TasksContext";
-import { Plus } from "lucide-react";
-import { useState } from "react";
 
 export default function Home() {
-  const { addTask } = useTasksContext()
-  const [description, setDescription] = useState<string>("")
-
-  const handleOnKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key == 'Escape') {
-      setDescription('')
-    }
-  }
-
-  const handleAddTask = async () => {
-    await addTask(description)
-  }
-
   return (
     <div className="min-h-screen w-full bg-blue-300 flex items-center justify-center">
       <div className="h-full min-w-xl flex flex-col items-center bg-gray-100 p-8 rounded-xl">
         <Header />
-        <div className="w-full flex flex-row items-center rounded-4xl bg-gray-200 mt-4">
-          <input
-            placeholder="Add your task"
-            value={description}
-            className="w-full  p-2 px-4 placeholder-gray-400 text-xl text-gray-700 outline-none" 
-            onChange={(e) => setDescription(e.target.value)} 
-            onKeyDown={handleOnKeyDown}/>
-
-          <button onClick={handleAddTask} >
-            <Plus className="bg-green-500 rounded-full text-gray-700 w-11 h-11 p-2 cursor-pointer" strokeWidth={3.5} />
-          </button>
-        </div>
+        <AddTaskInput />
         <TaskCardList />
       </div>
     </div>
--- components/AddTaskInput.tsx
+++ components/AddTaskInput.tsx
@@ -1,33 +1,42 @@
 'use client'
 
+import { useTasksContext } from '@/context/TasksContext'
 import { clientsApi } from '@/lib/api/client'
 import { Plus } from 'lucide-react'
 import React, { useState } from 'react'
 
-const AddTaskInput = async() => {
-  const [description, setDescription] = useState<string>('')
-  const clients = await clientsApi.getAll()
+const AddTaskInput = () => {
+  const { addTask } = useTasksContext()
+  const [description, setDescription] = useState<string>("")
 
-  const handleCreateTask = () => {
-    console.log("koe")
+  const handleOnKeyDown = async (e: React.KeyboardEvent) => {
+    if (e.key == 'Escape') {
+      setDescription('')
+    }
+    if (e.key == 'Enter') {
+      await addTask(description)
+      setDescription('')
+    }
+  }
+
+  const handleAddTask = async () => {
+    await addTask(description)
+    setDescription('')
   }
 
   return (
     <div className="w-full flex flex-row items-center rounded-4xl bg-gray-200 mt-4">
-          <input 
-            placeholder="Add your task" 
-            className="w-full  p-2 px-4 placeholder-gray-400 text-xl" 
-            onChange={(e) => setDescription(e.target.value)}
-            />
+      <input
+        placeholder="Add your task"
+        value={description}
+        className="w-full  p-2 px-4 placeholder-gray-400 text-xl text-gray-700 outline-none"
+        onChange={(e) => setDescription(e.target.value)}
+        onKeyDown={handleOnKeyDown} />
 
-          
-          <Plus 
-          className="bg-green-500 rounded-full text-gray-700 w-11 h-11 p-2" 
-          strokeWidth={3.5} 
-          onClick={handleCreateTask}
-          />
-        
-        </div>
+      <button onClick={handleAddTask} >
+        <Plus className="bg-green-500 rounded-full text-gray-700 w-11 h-11 p-2 cursor-pointer" strokeWidth={3.5} />
+      </button>
+    </div>
   )
 }
 
